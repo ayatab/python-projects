5507cbd73ece2008228d5fed5cb6228b552f5602
ayatab <42857860+ayatab@users.noreply.github.com>
2021-04-29 18:20:15 -0700

    making sudoku game

---
+++ sudoku.py
@@ -0,0 +1,36 @@
+# sudoku solver with gui
+
+# ALGORITHM: try all numbers and find a number that works, then move onto the next one.
+# Keep repeating until the solution cannot be completed, and go back one square and try all numbers possible again.
+# Repeat the "going backwards" algorithm until a line is completed.
+
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+
+# def is how you make functions in python
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - - - -")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+                if j == 8:
+                    print(board[i][j])
+                else:
+                    print(str(board[i][j]) + " ", end="")
+
+
+print_board(board)
